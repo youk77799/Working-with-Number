f90fa03750197713bce06b3081f2fd6384101f22
youk77799 <63965544+youk77799@users.noreply.github.com>
2022-01-25 16:49:10 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+print(-2.0987)
+
+# print a number just by putting a number between parenthesis
+
+print(3 + 4.5)
+
+# Mathematic (+) addition (-) subtraction (/) division (*) Multiplication
+# () parenthesis (%)Mod
+
+my_num = -5
+print(str(my_num) + " is my favorite number")
+
+# "str" is a function to convert number to a string
+
+print(abs(my_num))
+
+# "abs" turn negative number to normal
+
+print(pow(3, 2))
+
+# "pow" power the number
+
+print(max(4, 6))
+
+# print the highest number on the list,use "min" for opposite result
+
+print(round(3.2))
+
+# "round" convert <0.5 to 0 and >0.5 to 1
+
+
